b5293db0487ba79f8d984d644c8751d5663bbe36
Higor Vieira <74118081+Malaquiaz@users.noreply.github.com>
2025-12-09 11:02:07 -0300

    Correção da conexão com banco de dados

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from 'prisma/generated/prisma/client';
+import "dotenv/config";
+import { Injectable, Logger, OnModuleInit} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({
-      url: process.env.DATABASE_URL
-    });
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
     super({ adapter });
   }
+
+  async onModuleInit() {
+    try {
+      await this.$connect;
+      Logger.log('Conexão com o banco de dados estabelecida');
+    }
+    catch (error){
+      Logger.error('Falha na conexão com o Banco de dados, error');
+      throw error;
+    }
+  }
 }
+
+//import { PrismaPg } from '@prisma/adapter-pg'
+//import { PrismaClient } from '@prisma/client'
+
+//const connectionString = `${process.env.DATABASE_URL}`
+
+//const adapter = new PrismaPg({ connectionString })
+//const prisma = new PrismaClient({ adapter })
