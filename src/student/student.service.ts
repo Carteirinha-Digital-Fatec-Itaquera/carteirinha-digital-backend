586ef170161cd8d4b20bab2b0b8106639095439d
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2026-03-26 11:51:46 -0300

    Feat: Metodos FindById, Delete adicionados

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -3,6 +3,7 @@ import { CreateStudentDTO } from './dto/create-student.dto';
 import { StudentEntity } from './entities/student.entity';
 import { StudentMapper } from './mapper/student.mapper';
 import { StudentRepository } from './repository/student.repository';
+import { error } from 'console';
 
 @Injectable()
 export class StudentService {
@@ -12,6 +13,16 @@ export class StudentService {
   ) {}
   async getStudents(): Promise<StudentEntity[]> {
     return (await this.repository.findAll()).map((student) => student);
+  }
+
+  //
+  async getStudentsById(id: string): Promise<StudentEntity>{
+    const buscaId = await this.repository.findById(id)
+    if (!buscaId){
+      throw new error('Estudante não encontrado', NotFoundException)
+    }
+    
+    return buscaId
   }
 
   async getStudentByRa(ra: string): Promise<StudentEntity> {
@@ -32,5 +43,26 @@ export class StudentService {
 
   async createStudent(student: CreateStudentDTO) {
     await this.repository.create(this.mapper.toEntity(student));
+  }
+//
+  async deleteStudent(id: string) {
+    const busca = await this.repository.findById(id)
+
+    if(!busca){
+      throw new error('Usuario não encontrado', NotFoundException)
+    }
+    return await this.repository.delete(id)
+
+  }
+  async updateStudent(student: StudentEntity) {
+    const buscaStudents = await this.repository.findById(student.id)
+
+    if(!buscaStudents || student.id == null){
+      throw new error('Estudante não encontrado', NotFoundException)
+    }
+
+    return await this.repository.update(this.mapper.toEntity(student))
+
+
   }
 }
