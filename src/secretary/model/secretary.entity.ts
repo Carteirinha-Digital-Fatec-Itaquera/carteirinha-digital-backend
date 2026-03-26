35a45ab739ad94c09df23eb9c0d8912d02c86493
Lucas Garcia <lucasgarciax2@gmail.com>
2026-03-26 11:37:17 -0300

    fix: corrige tipagem do id para number conforme Prisma

--- src/secretary/model/secretary.entity.ts
+++ src/secretary/model/secretary.entity.ts
@@ -1,12 +1,12 @@
 export class SecretaryEntity {
-  constructor(id: string, name: string, email: string, password: string) {
+  constructor(id: number, name: string, email: string, password: string) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
   }
 
-  id: string;
+  id: number;
   name: string;
   email: string;
   password: string;
