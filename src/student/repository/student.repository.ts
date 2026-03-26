18f8774a4e20b8787aceaeb1fb94705fc17585ca
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2026-03-26 11:46:01 -0300

    Feat: metódos update, delete findById adicionados

--- src/student/repository/student.repository.ts
+++ src/student/repository/student.repository.ts
@@ -3,9 +3,15 @@ import { StudentEntity } from '../entities/student.entity';
 export abstract class StudentRepository {
   abstract findAll(): Promise<StudentEntity[]>;
 
+  abstract findById(id: string): Promise<StudentEntity | null> //
+
   abstract findByRa(ra: string): Promise<StudentEntity | null>;
 
   abstract findByEmail(email: string): Promise<StudentEntity | null>;
 
-  abstract create(student: StudentEntity): Promise<void>;
+  abstract create(student: StudentEntity): Promise<void>;//
+
+  abstract update(student: StudentEntity): Promise<void>;
+
+  abstract delete(id: string): Promise<void>;
 }
