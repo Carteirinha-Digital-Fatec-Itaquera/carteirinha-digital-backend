94a0e4d7f8eea3e5df078273f315a112ba7353e8
=d-jhon-b <=wwluiza.09@gmail.com>
2026-03-31 11:33:41 -0300

    adicionando hash de senhas

---
+++ src/utils/hashContent.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
+import TestAgent from 'supertest/lib/agent';
+
+
+@Injectable()
+export class HashContentService{
+
+    private readonly SALT_ROUNDS = 10;
+    async hashContent(password?: string, content?:string):Promise<string>{
+        const valuesToHash = password || content
+        if (!valuesToHash){
+            return "valor invalido"
+        }
+        
+        const values = bcrypt.hash(valuesToHash, this.SALT_ROUNDS);
+        return values;
+    }
+    async compareHash( valuesToCompare:string, password?:string, content?:string):Promise<boolean>{
+        const valuesComparreHash = password || content
+        if(!valuesComparreHash){
+            console.log("valor invalido ou nulo")
+            return false
+        }
+        return bcrypt.compare(valuesComparreHash, valuesToCompare)
+        // console.log("verficação relizada com sucesso")
+        // return true
+    }
+}
+
+
+// const value = new HashContent()
+
+// async function testBcrypt() {
+//     let passwordTeste = 'jhon'
+//     try{
+//         const res = await value.hashPassowrd(passwordTeste)
+//         console.log(res)
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
