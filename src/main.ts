777a04c45bb1c8cd31a60559536e15f7ae9f53d0
Lucas Garcia Lima <lucasgarciax2@gmail.com>
2026-05-10 00:59:02 -0300

    Refactor CORS configuration in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      '*',
-      'http://localhost:5173',
-      'http://localhost:4173',
-      'https://carteirinha-digital-front-end-secre-azure.vercel.app',
-      'https://carteirinha-digital-front-end-aluno-alpha.vercel.app'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:4173',
+    'https://carteirinha-digital-front-end-secre-azure.vercel.app',
+    'https://carteirinha-digital-front-end-aluno-alpha.vercel.app',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
 
   app.useGlobalPipes(new ValidationPipe());
 
