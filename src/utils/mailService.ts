496c31119abb2d586b9173046995685731cfeac7
Lucas Garcia Lima <lucasgarciax2@gmail.com>
2026-05-11 14:29:12 -0300

    Change Brevo environment to Default in mailService

--- src/utils/mailService.ts
+++ src/utils/mailService.ts
@@ -8,7 +8,7 @@ export class MailService {
   constructor() {
     this.client = new BrevoClient({
       apiKey: process.env.BREVO_API_KEY ?? '',
-      environment: BrevoEnvironment.Production,
+      environment: BrevoEnvironment.Default,
     });
   }
 
